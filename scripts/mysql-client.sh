2674aef756d986ae996d6816599f1ea3cdfb6278
Michael Dockter <michael@dockter.com>
2019-05-17 15:05:11 -0400

    Savepoint

--- scripts/mysql-client.sh
+++ scripts/mysql-client.sh
@@ -13,6 +13,7 @@ yum -y install \
 # Work-around https://senzing.zendesk.com/hc/en-us/articles/360009212393-MySQL-V8-0-ODBC-client-alongside-V5-x-Server
 
 TARGET_FILE=/opt/senzing/g2/lib/libmysqlclient.so.21
+mkdir -p /opt/senzing/g2/lib/
 
 wget https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-libs-8.0.12-1.el7.x86_64.rpm
 rpm2cpio mysql-community-libs-8.0.12-1.el7.x86_64.rpm | cpio -idmv
